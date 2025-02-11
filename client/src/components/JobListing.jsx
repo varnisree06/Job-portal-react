423ec8849b56860b8b484a8258dc1b095a41224a
varni06 <sakthidharanhiteshkumar@gmail.com>
2025-02-11 15:54:19 +0530

    initial commit

---
+++ client/src/components/JobListing.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const JobListing = () => {
+  return (
+    <div>
+       <div>
+        
+       </div>
+    </div>
+  )
+}
+
+export default JobListing
